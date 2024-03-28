d3bd16c433e60b8d720e124fc4f51368527588ce
Радмир Разяпов <abit.radmir@mail.ru>
2024-03-28 18:36:49 +0300

    add: button12

--- Lesson 28.03/resource.h
+++ Lesson 28.03/resource.h
@@ -19,6 +19,7 @@
 #define IDC_BUTTON9                     1009
 #define IDC_BUTTON10                    1010
 #define IDC_BUTTON11                    1011
+#define IDC_BUTTON12                    1012
 
 // Next default values for new objects
 // 
@@ -26,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
